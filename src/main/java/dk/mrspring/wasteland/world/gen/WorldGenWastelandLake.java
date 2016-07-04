0aa1ba47ac79f5f8e8bf305c56d1249d163a8ef9
MrSpring <mrspring@mrspring.dk>
2016-07-04 18:56:19 +0200

    Converted WorldGenWastelandLake

--- src/main/java/dk/mrspring/wasteland/world/gen/WorldGenWastelandLake.java
+++ src/main/java/dk/mrspring/wasteland/world/gen/WorldGenWastelandLake.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.init.Blocks;
+import net.minecraft.util.BlockPos;
 import net.minecraft.world.EnumSkyBlock;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenerator;
@@ -19,28 +20,27 @@ public class WorldGenWastelandLake extends WorldGenerator {
       this.field_150556_a = p_i45455_1_;
    }
 
-   public boolean generate(World p_76484_1_, Random p_76484_2_, int p_76484_3_, int p_76484_4_, int p_76484_5_) {
-      p_76484_3_ -= 8;
+   public boolean generate(World world, Random random, BlockPos pos) {
 
-      for(p_76484_5_ -= 8; p_76484_4_ > 5 && p_76484_1_.isAirBlock(p_76484_3_, p_76484_4_, p_76484_5_); --p_76484_4_) {
+      for(pos.add(-8, 0, -8); pos.getY() > 5 && world.isAirBlock(pos); pos.add(0, -1, 0)) {
          ;
       }
 
-      if(p_76484_4_ <= 4) {
+      if(pos.getY() <= 4) {
          return false;
       } else {
-         p_76484_4_ -= 4;
+         pos.add(0, -4, 0);
          boolean[] aboolean = new boolean[2048];
-         int l = p_76484_2_.nextInt(4) + 4;
+         int l = random.nextInt(4) + 4;
 
          int i1;
          for(i1 = 0; i1 < l; ++i1) {
-            double j1 = p_76484_2_.nextDouble() * 6.0D + 3.0D;
-            double flag = p_76484_2_.nextDouble() * 4.0D + 2.0D;
-            double d2 = p_76484_2_.nextDouble() * 6.0D + 3.0D;
-            double d3 = p_76484_2_.nextDouble() * (16.0D - j1 - 2.0D) + 1.0D + j1 / 2.0D;
-            double d4 = p_76484_2_.nextDouble() * (8.0D - flag - 4.0D) + 2.0D + flag / 2.0D;
-            double d5 = p_76484_2_.nextDouble() * (16.0D - d2 - 2.0D) + 1.0D + d2 / 2.0D;
+            double j1 = random.nextDouble() * 6.0D + 3.0D;
+            double flag = random.nextDouble() * 4.0D + 2.0D;
+            double d2 = random.nextDouble() * 6.0D + 3.0D;
+            double d3 = random.nextDouble() * (16.0D - j1 - 2.0D) + 1.0D + j1 / 2.0D;
+            double d4 = random.nextDouble() * (8.0D - flag - 4.0D) + 2.0D + flag / 2.0D;
+            double d5 = random.nextDouble() * (16.0D - d2 - 2.0D) + 1.0D + d2 / 2.0D;
 
             for(int k1 = 1; k1 < 15; ++k1) {
                for(int l1 = 1; l1 < 15; ++l1) {
@@ -65,12 +65,12 @@ public class WorldGenWastelandLake extends WorldGenerator {
                for(var32 = 0; var32 < 8; ++var32) {
                   var33 = !aboolean[(i1 * 16 + j2) * 8 + var32] && (i1 < 15 && aboolean[((i1 + 1) * 16 + j2) * 8 + var32] || i1 > 0 && aboolean[((i1 - 1) * 16 + j2) * 8 + var32] || j2 < 15 && aboolean[(i1 * 16 + j2 + 1) * 8 + var32] || j2 > 0 && aboolean[(i1 * 16 + (j2 - 1)) * 8 + var32] || var32 < 7 && aboolean[(i1 * 16 + j2) * 8 + var32 + 1] || var32 > 0 && aboolean[(i1 * 16 + j2) * 8 + (var32 - 1)]);
                   if(var33) {
-                     Material b0 = p_76484_1_.getBlock(p_76484_3_ + i1, p_76484_4_ + var32, p_76484_5_ + j2).getMaterial();
+                     Material b0 = world.getBlockState(pos.add(i1, var32, j2)).getBlock().getMaterial();
                      if(var32 >= 4 && b0.isLiquid()) {
                         return false;
                      }
 
-                     if(var32 < 4 && !b0.isSolid() && p_76484_1_.getBlock(p_76484_3_ + i1, p_76484_4_ + var32, p_76484_5_ + j2) != this.field_150556_a) {
+                     if(var32 < 4 && !b0.isSolid() && world.getBlockState(pos.add(i1, var32, j2)).getBlock() != this.field_150556_a) {
                         return false;
                      }
                   }
@@ -82,7 +82,7 @@ public class WorldGenWastelandLake extends WorldGenerator {
             for(j2 = 0; j2 < 16; ++j2) {
                for(var32 = 0; var32 < 8; ++var32) {
                   if(aboolean[(i1 * 16 + j2) * 8 + var32]) {
-                     p_76484_1_.setBlock(p_76484_3_ + i1, p_76484_4_ + var32, p_76484_5_ + j2, var32 >= 4?Blocks.air:this.field_150556_a, 0, 2);
+                     world.setBlockState(pos.add(i1, var32, j2), var32 >= 4?Blocks.air.getDefaultState():this.field_150556_a.getDefaultState(), 2);
                   }
                }
             }
@@ -91,8 +91,8 @@ public class WorldGenWastelandLake extends WorldGenerator {
          for(i1 = 0; i1 < 16; ++i1) {
             for(j2 = 0; j2 < 16; ++j2) {
                for(var32 = 4; var32 < 8; ++var32) {
-                  if(aboolean[(i1 * 16 + j2) * 8 + var32] && p_76484_1_.getBlock(p_76484_3_ + i1, p_76484_4_ + var32 - 1, p_76484_5_ + j2) == ModConfig.getSurfaceBlock() && p_76484_1_.getSavedLightValue(EnumSkyBlock.Sky, p_76484_3_ + i1, p_76484_4_ + var32, p_76484_5_ + j2) > 0) {
-                     p_76484_1_.setBlock(p_76484_3_ + i1, p_76484_4_ + var32 - 1, p_76484_5_ + j2, ModConfig.getSurfaceBlock(), 0, 2);
+                  if(aboolean[(i1 * 16 + j2) * 8 + var32] && world.getBlockState(pos.add(i1, var32 - 1, j2)).getBlock() == ModConfig.getSurfaceBlock() && world.getLightFor(EnumSkyBlock.SKY, pos.add(i1, var32, j2)) > 0) {
+                     world.setBlockState(pos.add(i1, var32 - 1, j2), ModConfig.getSurfaceBlock().getDefaultState(), 2);
                   }
                }
             }
@@ -103,8 +103,8 @@ public class WorldGenWastelandLake extends WorldGenerator {
                for(j2 = 0; j2 < 16; ++j2) {
                   for(var32 = 0; var32 < 8; ++var32) {
                      var33 = !aboolean[(i1 * 16 + j2) * 8 + var32] && (i1 < 15 && aboolean[((i1 + 1) * 16 + j2) * 8 + var32] || i1 > 0 && aboolean[((i1 - 1) * 16 + j2) * 8 + var32] || j2 < 15 && aboolean[(i1 * 16 + j2 + 1) * 8 + var32] || j2 > 0 && aboolean[(i1 * 16 + (j2 - 1)) * 8 + var32] || var32 < 7 && aboolean[(i1 * 16 + j2) * 8 + var32 + 1] || var32 > 0 && aboolean[(i1 * 16 + j2) * 8 + (var32 - 1)]);
-                     if(var33 && (var32 < 4 || p_76484_2_.nextInt(2) != 0) && p_76484_1_.getBlock(p_76484_3_ + i1, p_76484_4_ + var32, p_76484_5_ + j2).getMaterial().isSolid()) {
-                        p_76484_1_.setBlock(p_76484_3_ + i1, p_76484_4_ + var32, p_76484_5_ + j2, Blocks.stone, 0, 2);
+                     if(var33 && (var32 < 4 || random.nextInt(2) != 0) && world.getBlockState(pos.add(i1, var32, j2)).getBlock().getMaterial().isSolid()) {
+                        world.setBlockState(pos.add(i1, var32, j2), Blocks.stone.getDefaultState(), 2);
                      }
                   }
                }
@@ -115,8 +115,8 @@ public class WorldGenWastelandLake extends WorldGenerator {
             for(i1 = 0; i1 < 16; ++i1) {
                for(j2 = 0; j2 < 16; ++j2) {
                   byte var34 = 4;
-                  if(p_76484_1_.isBlockFreezable(p_76484_3_ + i1, p_76484_4_ + var34, p_76484_5_ + j2)) {
-                     p_76484_1_.setBlock(p_76484_3_ + i1, p_76484_4_ + var34, p_76484_5_ + j2, Blocks.ice, 0, 2);
+                  if(world.canBlockFreezeWater(pos.add(i1, var34, j2))) {
+                     world.setBlockState(pos.add(i1, var34, j2), Blocks.ice.getDefaultState(), 2);
                   }
                }
             }
